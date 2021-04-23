2a43a17f0e5bcd6566db713a3a6ed1267a4f1716
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2021-04-23 22:09:48 +0200

    Use Express to Create a Server V2.0

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ const express = require('express')
 
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '../public'))
-app.set('views', 'views');
+app.use(express.static(__dirname + 'public'))
+
 
 app.get('/', (req, res, next) => {
-  res.render(path.join(__dirname, 'views', 'index.ejs'), { title: 'Index' })
+  res.render('index', { title: 'Index' })
 })
 
 app.listen(3000, () => {
